c6586e9d37e4ca2eaa0d415e4c8275e2dafc91b9
Tim Bartsch <timbartsch.de@gmail.com>
2014-08-28 23:15:33 +0200

    Hiding now info text initially.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(function(){
+  $(".row.warning").click(function(){
+    var $info = $(this).children(".info");
+    if($info.is(":visible")){
+      $info.slideUp();
+    } else {
+      $info.slideDown();
+    }
+  });
+});
